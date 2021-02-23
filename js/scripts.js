4220cc5e51ab39fe247cf6ce5ee4c00b517a03dd
fischbroetchen <fischbroetchens@gmail.com>
2021-02-23 14:17:40 +0100

    faq scroll fix

--- js/scripts.js
+++ js/scripts.js
@@ -112,3 +112,22 @@
             element2.classList.remove("slider-toggle");
             }
 
+jQuery(function($){
+  $('.card-header').each(function(){
+    let $card_header = $(this);
+    let $collapse_element = $card_header.next();
+    $collapse_element.on('show.bs.collapse', function () {
+      let $card_header_top = $card_header.offset().top-100;
+      let $visible_collapse = $('.collapse.show');
+      if($visible_collapse.length){
+        let $visible_collapse_top = $visible_collapse.offset().top+100;
+        if($visible_collapse_top < $card_header_top){
+          $card_header_top -= $visible_collapse.height();
+        }
+      }
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $card_header_top
+      });
+    });
+  });
+});
